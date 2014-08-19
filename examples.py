c835765969c80e8263ac8eaa1621cc47363e0893
David Sterry <davids@exchb.com>
2014-08-19 01:06:55 -0400

    more queries and better formatting

--- examples.py
+++ examples.py
@@ -9,16 +9,23 @@ home = expanduser("~")
 conn = sqlite3.connect(home+'/.config/dogepartyd/dogepartyd.9.db')
 c = conn.cursor()
 
-# get lock status of an asset
-asset = ("DOGEPARTYTALK",)
-c.execute('SELECT asset, locked FROM issuances where asset = ? order by tx_index desc limit 1',asset)
-print (c.fetchone())
-
+# global statistics
 # get number of assets registered
 c.execute('select count(distinct(asset)) from issuances;')
-print (c.fetchone())
+print ("Total assets: "+str(c.fetchone()[0]))
+
+# get number of assets that have been locked 
+c.execute('select count(distinct(asset)) from issuances where locked = 1;')
+print ("Locked assets: "+str(c.fetchone()[0]))
 
 # get number of assets that have had a quantity sent at least once
 c.execute('select count(distinct(issuances.asset)) from issuances inner join sends on sends.asset = issuances.asset;')
-print (c.fetchone())
+print ("Assets distributed at least once: "+str(c.fetchone()[0]))
 
+# queries about an asset
+# get lock status of an asset
+asset = ("DOGEPARTYTALK",)
+c.execute('SELECT asset, locked FROM issuances where asset = ? order by tx_index desc limit 1',asset)
+row = c.fetchone()
+print ("\nAsset name: "+str(row[0]))
+print ("Locked: "+str(row[1]))
